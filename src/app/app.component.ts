157951bd3325f7b7c1f64661f932eb12428c703d
M. Algarni <m.t.algarni@gmail.com>
2017-03-19 15:40:34 +0300

    Office 19-03-2017

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+  cuisines: FirebaseListObservable<any[]>;
+  restaurants: FirebaseListObservable<any[]>;
+
+  constructor(private af: AngularFire) {
+
+  }
+
+  ngOnInit() {
+    this.cuisines = this.af.database.list('/cuisines');
+    this.restaurants = this.af.database.list('/restaurants');
+  }
+
+  add(){
+    this.cuisines.push({
+      name: 'Asain'
+    });
+  }
+
+  update(){
+    this.af.database.object('/restaurants')
+  }
+
+  remove(){
+    this.af.database.object('/restaurant').remove()
+    .then(x => console.log("SUCCESS"))
+    .catch(error => console.log('ERROR'))
+  }
+  
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBdMu_o54utDkwR0YJSlFszQTSZ8iIMmOE",
+  authDomain: "pepper-d218e.firebaseapp.com",
+  databaseURL: "https://pepper-d218e.firebaseio.com",
+  storageBucket: "pepper-d218e.appspot.com",
+  messagingSenderId: "137162589373"
+
+}
 
 @NgModule({
   declarations: [
@@ -12,7 +22,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
